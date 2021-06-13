be6e0075d679bf8fb8b5853c11dbbfce8c202e92
moriamislam7423 <85065261+moriamislam7423@users.noreply.github.com>
2021-06-13 23:05:34 +0600

    Add files via upload

---
+++ Bookshop management system.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+void title()
+{
+    cout<<"-------------------------------------\n";
+    //cout/<<"|\n";
+    cout<<"|Welcome to General bookshop "<<endl;
+    cout<<"|\n";
+    cout<<"--------------------------------------\n";
+    cout<<"\n\n\n";
+    cout<<"1.Customer"<<endl;
+    cout<<"2.Manager"<<endl;
+    cout<<"Choose any option please:";
+}
+
+int main()
+{
+    title();
+    //menu();
+    return main();
+}
